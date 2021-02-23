bcf6b73127c1a3a6c60e8ea267deaf33ed021d13
Peter Dore <doredesign@gmail.com>
2021-02-22 22:30:02 -0800

    Add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import '@babel/polyfill';
 import App from './App.svelte';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
 
 new App({
   target: document.getElementById('demo'),
